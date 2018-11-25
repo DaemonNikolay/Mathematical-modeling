8ebc4ce3552d2ccafe868549e4799b7d993b1c88
DaemonNikolay <nikolay-eckert@mail.ru>
2018-11-25 13:01:10 +0700

    Create append table - non-stable

--- MathModeliing/TransportTask.cs
+++ MathModeliing/TransportTask.cs
@@ -8,6 +8,13 @@ namespace MathModeliing
 {
     public class TransportTask
     {
+        private List<int[]> table;
+
+        public TransportTask(int[] A1, int[] A2, int[] A3, int[] A4, int[] Needs)
+        {
+
+        }
+
         public double MethodPotential()
         {
             return double.MinValue;
